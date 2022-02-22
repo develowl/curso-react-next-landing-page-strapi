5a6062b99db37489537d910ed7387202f8197629
fgyan94 <fgyan94@gmail.com>
2022-02-21 23:26:53 -0300

    Retomando o curso

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '721f71665c256119bf711fd80b348212'),
+      secret: env('ADMIN_JWT_SECRET', 'WWZoaC5fmaZX3SS25sbwRO9n4nRXutIfu8bkkqud8Bx2ZRBKw6pryjrimoPo9TOcaUszxTeqCxXnQomtek7c0A=='),
     },
   },
 });
